2fdefcc7c85c8a0b134046e249247e790e1753c8
James Price <j.price@bristol.ac.uk>
2013-11-25 22:19:34 +0000

    Fixed indentation and improved comments in half.h.

--- src/spirsim/half.h
+++ src/spirsim/half.h
@@ -18,105 +18,116 @@
 
 float halfToFloat(uint16_t half)
 {
-	uint16_t h_sign, h_exponent, h_mantissa;
-	uint32_t f_sign, f_exponent, f_mantissa;
+  uint16_t h_sign, h_exponent, h_mantissa;
+  uint32_t f_sign, f_exponent, f_mantissa;
 
-	h_sign     = half & 0x8000; // 1000 0000 0000 0000
-	h_exponent = half & 0x7C00; // 0111 1100 0000 0000
-	h_mantissa = half & 0x03FF; // 0000 0011 1111 1111
+  h_sign     = half & 0x8000; // 1000 0000 0000 0000
+  h_exponent = half & 0x7C00; // 0111 1100 0000 0000
+  h_mantissa = half & 0x03FF; // 0000 0011 1111 1111
 
-	f_sign     = ((uint32_t)h_sign) << 16;
+  f_sign     = ((uint32_t)h_sign) << 16;
 
-	if (h_exponent == 0)
-	{
-		if (h_mantissa == 0)
-		{
-			f_exponent = 0;
-			f_mantissa = 0;
-		}
-		else
-		{
-			int e = -1;
-			do
-			{
-				e++;
-				h_mantissa <<= 1;
-			}
-			while((h_mantissa & 0x0400) == 0);
+  if (h_exponent == 0)
+  {
+    if (h_mantissa == 0)
+    {
+      // Zero
+      f_exponent = 0;
+      f_mantissa = 0;
+    }
+    else
+    {
+      // Denorm - convert to normalized float
+      int e = -1;
+      do
+      {
+        e++;
+        h_mantissa <<= 1;
+      }
+      while((h_mantissa & 0x0400) == 0);
 
-			f_exponent = (-15 + 127 - e) << 23;
-			f_mantissa = ((uint32_t)(h_mantissa & 0x03FF)) << 13;
-		}
-	}
-	else if (h_exponent == 0x7C00)
-	{
-		f_exponent = 0xFF << 23;
-		f_mantissa = h_mantissa;
-	}
-	else
-	{
-		f_exponent = (((int32_t)(h_exponent >> 10)) - 15 + 127) << 23;
-		f_mantissa = ((uint32_t)h_mantissa) << 13;
-	}
+      f_exponent = (-15 + 127 - e) << 23;
+      f_mantissa = ((uint32_t)(h_mantissa & 0x03FF)) << 13;
+    }
+  }
+  else if (h_exponent == 0x7C00)
+  {
+    // Inf or NaN
+    f_exponent = 0xFF << 23;
+    f_mantissa = h_mantissa;
+  }
+  else
+  {
+    // Normalized
+    f_exponent = (((int32_t)(h_exponent >> 10)) - 15 + 127) << 23;
+    f_mantissa = ((uint32_t)h_mantissa) << 13;
+  }
 
-	uint32_t result = f_sign | f_exponent | f_mantissa;
-	return *(float*)&result;
+  uint32_t result = f_sign | f_exponent | f_mantissa;
+  return *(float*)&result;
 }
 
 uint16_t floatToHalf(float sp)
 {
-	uint16_t h_sign, h_exponent, h_mantissa;
-	uint32_t f_sign, f_exponent, f_mantissa;
+  uint16_t h_sign, h_exponent, h_mantissa;
+  uint32_t f_sign, f_exponent, f_mantissa;
 
-	uint32_t f = *(uint32_t*)&sp;
-	f_sign     = f & 0x80000000; // 1000 0000 0000 0000 0000 0000 0000 0000
-	f_exponent = f & 0x7F800000; // 0111 1111 1000 0000 0000 0000 0000 0000
-	f_mantissa = f & 0x007FFFFF; // 0000 0000 0111 1111 1111 1111 1111 1111
+  uint32_t f = *(uint32_t*)&sp;
+  f_sign     = f & 0x80000000; // 1000 0000 0000 0000 0000 0000 0000 0000
+  f_exponent = f & 0x7F800000; // 0111 1111 1000 0000 0000 0000 0000 0000
+  f_mantissa = f & 0x007FFFFF; // 0000 0000 0111 1111 1111 1111 1111 1111
 
-	h_sign     = f_sign >> 16;
+  h_sign     = f_sign >> 16;
 
-	if (f_exponent == 0)
-	{
-		h_exponent = 0;
-		h_mantissa = 0;
-	}
-	else if (f_exponent == 0x7F800000)
-	{
-		h_exponent = 0x7C00;
-		h_mantissa = f_mantissa;
-	}
-	else
-	{
-		int e = (((int32_t)(f_exponent >> 23)) - 127 + 15);
-		if (e >= 0x1F)
-		{
-			h_exponent = 0x7C00;
-			h_mantissa = 0;
-		}
-		else if (e <= 0)
-		{
-			h_exponent = 0;
-			if (14 - e > 24)
-			{
-				h_mantissa = 0;
-			}
-			else
-			{
-				f_mantissa |= 0x800000;
-				h_mantissa = (f_mantissa >> (14-e));
-				if ((f_mantissa >> (13 - e)) & 0x1)
-				{
-					h_mantissa += 0x1;
-				}
-			}
-		}
-		else
-		{
-			// TODO: Round
-			h_exponent = e << 10;
-			h_mantissa = f_mantissa >> 13;
-		}
-	}
+  if (f_exponent == 0)
+  {
+    // Zero
+    h_exponent = 0;
+    h_mantissa = 0;
+  }
+  else if (f_exponent == 0x7F800000)
+  {
+    // Inf or NaN
+    h_exponent = 0x7C00;
+    h_mantissa = f_mantissa;
+  }
+  else
+  {
+    int e = (((int32_t)(f_exponent >> 23)) - 127 + 15);
+    if (e >= 0x1F)
+    {
+      // Value will overflow
+      h_exponent = 0x7C00;
+      h_mantissa = 0;
+    }
+    else if (e <= 0)
+    {
+      // Value will underflow
+      h_exponent = 0;
+      if (14 - e > 24)
+      {
+        // Too small - flush to zero
+        h_mantissa = 0;
+      }
+      else
+      {
+        // Convert to denorm
+        f_mantissa |= 0x800000;
+        h_mantissa = (f_mantissa >> (14-e));
+        if ((f_mantissa >> (13 - e)) & 0x1)
+        {
+          h_mantissa += 0x1;
+        }
+      }
+    }
+    else
+    {
+      // Normalized
+      // TODO: Round appropriately
+      h_exponent = e << 10;
+      h_mantissa = f_mantissa >> 13;
+    }
+  }
 
-	return h_sign + h_exponent + h_mantissa;
+  return h_sign + h_exponent + h_mantissa;
 }
