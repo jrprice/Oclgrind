682496747ea35dc005da8f3e33e7cd9a54014e98
James Price <j.price@bristol.ac.uk>
2014-02-23 19:05:25 +0000

    Switched to a three-clause BSD license.

--- src/spirsim/Kernel.h
+++ src/spirsim/Kernel.h
@@ -1,18 +1,10 @@
 // Kernel.h (Oclgrind)
-// Copyright (C) 2013 James Price
+// Copyright (c) 2013-2014, University of Bristol
+// All rights reserved.
 //
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, see <http://www.gnu.org/licenses/>.
+// This program is provided under a three-clause BSD license. For full
+// license terms please see the LICENSE file distributed with this
+// source code.
 
 #include "common.h"
 
