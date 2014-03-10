8c89aa54a780d6f4a889cd8871466d32bdc8d40b
James Price <j.price@bristol.ac.uk>
2014-03-10 12:09:34 +0000

    Fixed CMake build for versions prior to 2.8.3.

--- src/spirsim/gen_clc_h.cmake
+++ src/spirsim/gen_clc_h.cmake
@@ -2,7 +2,7 @@ set(OUTPUT src/spirsim/clc_h.cpp)
 
 file(WRITE ${OUTPUT} "extern const char CLC_H_DATA[] = \n\"")
 
-file(READ ${CMAKE_CURRENT_LIST_DIR}/clc.h CLC_H)
+file(READ ${SOURCE_FILE} CLC_H)
 string(REGEX REPLACE "\\\\" "\\\\\\\\" CLC_H "${CLC_H}")
 string(REGEX REPLACE "\"" "\\\\\"" CLC_H "${CLC_H}")
 string(REGEX REPLACE "\n" "\\\\n\"\n\"" CLC_H "${CLC_H}")
