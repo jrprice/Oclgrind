f52afa39d90624561d640b33e058c6af886a383c
James Price <j.price@bristol.ac.uk>
2016-02-13 00:47:13 +0000

    Refactor test running scripts into single script
    This allows for app/runtime tests to use the .ref checking
    mechanism. The map_buffer test has been updated to include some forced
    'use while mapped' errors.

--- tests/run_test.py
+++ tests/run_test.py
@@ -1,57 +1,139 @@
 # run_test.py (Oclgrind)
-# Copyright (c) 2013-2015, James Price and Simon McIntosh-Smith,
+# Copyright (c) 2013-2016, James Price and Simon McIntosh-Smith,
 # University of Bristol. All rights reserved.
 #
 # This program is provided under a three-clause BSD license. For full
 # license terms please see the LICENSE file distributed with this
 # source code.
 
+import errno
 import os
 import re
 import subprocess
 import sys
 
 # Check arguments
-if len(sys.argv) != 2:
-  print 'Usage: python run_test.py EXE'
+if len(sys.argv) != 3:
+  print 'Usage: python run_test.py OCLGRIND-KERNEL TEST_EXE|TEST.sim'
   sys.exit(1)
-if not os.path.isfile(sys.argv[1]):
-  print 'Test executable not found'
+if not os.path.isfile(sys.argv[2]):
+  print 'Test file not found'
   sys.exit(1)
 
 # Construct paths to test inputs/outputs
-test_exe    = sys.argv[1]
-test_dir    = os.path.dirname(os.path.realpath(test_exe))
-test_name   = os.path.splitext(os.path.basename(test_exe))[0]
+oclgrind_kernel = sys.argv[1]
+test_full_path  = sys.argv[2]
+test_dir        = os.path.dirname(os.path.realpath(test_full_path))
+test_file       = os.path.basename(test_full_path)
+test_name       = os.path.splitext(test_file)[0]
+current_dir     = os.getcwd()
+
+if test_file.endswith('.sim'):
+  test_ref = test_full_path[:-4] + '.ref'
+else:
+  if test_full_path[0] == '/':
+    rel_path = test_full_path[test_full_path.find('/tests/') + 7:]
+  else:
+    rel_path = test_full_path
+
+  test_ref = os.path.dirname(os.path.abspath(__file__)) + os.path.sep \
+    + rel_path + '.ref'
+
+print current_dir
+print test_full_path
+print test_ref
 
 # Enable race detection and uninitialized memory plugins
 os.environ["OCLGRIND_CHECK_API"] = "1"
 os.environ["OCLGRIND_DATA_RACES"] = "1"
 os.environ["OCLGRIND_UNINITIALIZED"] = "1"
 
+def fail(ret=1):
+  print 'FAILED'
+  sys.exit(ret)
+
 def run(output_suffix):
 
-  test_out = test_dir + os.path.sep + test_name + output_suffix + '.out'
+  # Get filename for test output
+  if test_file.endswith('.sim'):
+    test_out = test_dir.split(os.path.sep)[-1] + os.path.sep + \
+               test_name + output_suffix + '.out'
+  else:
+    test_out = test_dir + os.path.sep + \
+               test_name + output_suffix + '.out'
+
+
+  output_dir = os.path.dirname(test_out)
+  try:
+    os.makedirs(output_dir)
+  except OSError as exc:
+    if exc.errno == errno.EEXIST and os.path.isdir(output_dir):
+      pass
+    else:
+      raise
+
+  out = open(test_out, 'w')
 
   # Run test
-  out = open(test_out, 'w')
-  retval = subprocess.call([test_exe], stdout=out, stderr=out)
+  if test_file.endswith('.sim'):
+    os.chdir(test_dir)
+    retval = subprocess.call([oclgrind_kernel, test_file],
+                             stdout=out, stderr=out)
+    os.chdir(current_dir)
+  else:
+    retval = subprocess.call([test_full_path], stdout=out, stderr=out)
+
   out.close()
   if retval != 0:
-    print test_name + ' returned non-zero value (' + str(retval) + ')'
-    sys.exit(retval)
+    print 'Test returned non-zero value (' + str(retval) + ')'
+    fail(retval)
 
-  # Open output file
-  out = open(test_out).read().splitlines()
+  # Compare output to reference file (if provided)
+  if os.path.isfile(test_ref):
 
-  # Check that an error was produced iff an error was expected
-  # Assume any text in output is an error
-  # TODO: Allow a test to require an error
-  # TODO: Improve this so that more details about the error are checked
-  if len(out) > 0:
-    print 'Error reported, but no error expected'
-    sys.exit(1)
+    # Open output and reference files
+    out = open(test_out).read().splitlines()
+    ref = open(test_ref).read().splitlines()
 
+    # Check output matches references
+    oi = 0
+    for line in ref:
+      if len(line) == 0:
+        continue
+
+      type = line.split()[0]
+      text = line[6:]
+
+      # Find next non-blank line in output file
+      while not len(out[oi]):
+        oi += 1
+
+      if type == 'ERROR':
+        # Check first line of error contains reference message
+        if not text in out[oi]:
+          print 'Expected '  + line
+          print 'Found    "' + out[oi] + '"'
+          fail()
+        # Skip remaining lines of error
+        while oi < len(out) and len(out[oi]):
+          oi += 1
+      elif type == 'EXACT':
+        # Check line of output matches reference exactly
+        if not text == out[oi]:
+          print 'Expected '  + line
+          print 'Found    "' + out[oi] + '"'
+          fail()
+        oi += 1
+      elif type == 'MATCH':
+        # Check line of output contains reference text
+        if not text in out[oi]:
+          print 'Expected '  + line
+          print 'Found    "' + out[oi] + '"'
+          fail()
+        oi += 1
+      else:
+        print 'Invalid match type in reference file'
+        fail()
 
 print 'Running test with optimisations'
 run('')
--- tests/runtime/map_buffer.c
+++ tests/runtime/map_buffer.c
@@ -18,32 +18,15 @@ const char *KERNEL_SOURCE =
 "}                                   \n"
 ;
 
-int main(int argc, char *argv[])
+unsigned checkResults(size_t N, float *a, float *b, float *results);
+
+// Run everything as normal
+unsigned run1(Context cl, cl_kernel kernel,
+              cl_mem d_a, cl_mem d_b, cl_mem d_c, size_t N)
 {
   cl_int err;
-  cl_kernel kernel;
-  cl_mem d_a, d_b, d_c;
   float *h_a, *h_b, *h_c;
-
-  size_t N = 1024;
-  if (argc > 1)
-  {
-    N = atoi(argv[1]);
-  }
-
-  Context cl = createContext(KERNEL_SOURCE);
-
-  kernel = clCreateKernel(cl.program, "vecadd", &err);
-  checkError(err, "creating kernel");
-
   size_t dataSize = N*sizeof(cl_float);
-
-  d_a = clCreateBuffer(cl.context, CL_MEM_READ_ONLY, dataSize, NULL, &err);
-  checkError(err, "creating d_a buffer");
-  d_b = clCreateBuffer(cl.context, CL_MEM_READ_ONLY, dataSize, NULL, &err);
-  checkError(err, "creating d_b buffer");
-  d_c = clCreateBuffer(cl.context, CL_MEM_WRITE_ONLY, dataSize, NULL, &err);
-  checkError(err, "creating d_c buffer");
 
   // Initialise data
   srand(0);
@@ -55,7 +38,129 @@ int main(int argc, char *argv[])
                            CL_MAP_WRITE_INVALIDATE_REGION,
                            0, dataSize, 0, NULL, NULL, &err);
   checkError(err, "mapping d_b buffer");
-  h_c = malloc(dataSize);
+  h_c = clEnqueueMapBuffer(cl.queue, d_c, CL_FALSE, CL_MAP_READ, 0, dataSize,
+                           0, NULL, NULL, &err);
+  checkError(err, "mapping d_c buffer");
+  for (unsigned i = 0; i < N; i++)
+  {
+    h_a[i] = rand()/(float)RAND_MAX;
+    h_b[i] = rand()/(float)RAND_MAX;
+    h_c[i] = 0;
+  }
+
+  err = clEnqueueUnmapMemObject(cl.queue, d_a, h_a, 0, NULL, NULL);
+  checkError(err, "unmapping d_a");
+  err = clEnqueueUnmapMemObject(cl.queue, d_b, h_b, 0, NULL, NULL);
+  checkError(err, "unmapping d_b");
+  err = clEnqueueUnmapMemObject(cl.queue, d_c, h_c, 0, NULL, NULL);
+  checkError(err, "unmapping d_c");
+
+  err  = clSetKernelArg(kernel, 0, sizeof(cl_mem), &d_a);
+  err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &d_b);
+  err |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &d_c);
+  checkError(err, "setting kernel args");
+
+  err = clEnqueueNDRangeKernel(cl.queue, kernel,
+                               1, NULL, &N, NULL, 0, NULL, NULL);
+  checkError(err, "enqueuing kernel");
+
+  h_c = clEnqueueMapBuffer(cl.queue, d_c, CL_FALSE, CL_MAP_READ, 0, dataSize,
+                           0, NULL, NULL, &err);
+  checkError(err, "mapping d_c buffer");
+
+  err = clFinish(cl.queue);
+  checkError(err, "running kernel");
+
+  unsigned errors = checkResults(N, h_a, h_b, h_c);
+
+  err = clEnqueueUnmapMemObject(cl.queue, d_c, h_c, 0, NULL, NULL);
+  checkError(err, "unmapping d_c");
+
+  return errors;
+}
+
+// Don't unmap input buffers before running kernel
+// Should result in "Invalid read from buffer mapped for writing" error
+unsigned run2(Context cl, cl_kernel kernel,
+              cl_mem d_a, cl_mem d_b, cl_mem d_c, size_t N)
+{
+  cl_int err;
+  float *h_a, *h_b, *h_c;
+  size_t dataSize = N*sizeof(cl_float);
+
+  // Initialise data
+  srand(0);
+  h_a = clEnqueueMapBuffer(cl.queue, d_a, CL_TRUE,
+                           CL_MAP_WRITE_INVALIDATE_REGION,
+                           0, dataSize, 0, NULL, NULL, &err);
+  checkError(err, "mapping d_a buffer");
+  h_b = clEnqueueMapBuffer(cl.queue, d_b, CL_TRUE,
+                           CL_MAP_WRITE_INVALIDATE_REGION,
+                           0, dataSize, 0, NULL, NULL, &err);
+  checkError(err, "mapping d_b buffer");
+  h_c = clEnqueueMapBuffer(cl.queue, d_c, CL_FALSE, CL_MAP_READ, 0, dataSize,
+                           0, NULL, NULL, &err);
+  checkError(err, "mapping d_c buffer");
+  for (unsigned i = 0; i < N; i++)
+  {
+    h_a[i] = rand()/(float)RAND_MAX;
+    h_b[i] = rand()/(float)RAND_MAX;
+    h_c[i] = 0;
+  }
+
+  err = clEnqueueUnmapMemObject(cl.queue, d_c, h_c, 0, NULL, NULL);
+  checkError(err, "unmapping d_c");
+
+  err  = clSetKernelArg(kernel, 0, sizeof(cl_mem), &d_a);
+  err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &d_b);
+  err |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &d_c);
+  checkError(err, "setting kernel args");
+
+  err = clEnqueueNDRangeKernel(cl.queue, kernel,
+                               1, NULL, &N, NULL, 0, NULL, NULL);
+  checkError(err, "enqueuing kernel");
+
+  h_c = clEnqueueMapBuffer(cl.queue, d_c, CL_FALSE, CL_MAP_READ, 0, dataSize,
+                           0, NULL, NULL, &err);
+  checkError(err, "mapping d_c buffer");
+
+  err = clFinish(cl.queue);
+  checkError(err, "running kernel");
+
+  unsigned errors = checkResults(N, h_a, h_b, h_c);
+
+  err = clEnqueueUnmapMemObject(cl.queue, d_a, h_a, 0, NULL, NULL);
+  checkError(err, "unmapping d_a");
+  err = clEnqueueUnmapMemObject(cl.queue, d_b, h_b, 0, NULL, NULL);
+  checkError(err, "unmapping d_b");
+  err = clEnqueueUnmapMemObject(cl.queue, d_c, h_c, 0, NULL, NULL);
+  checkError(err, "unmapping d_c");
+
+  return errors;
+}
+
+// Don't unmap output buffer before running kernel
+// Should result in "Invalid write to mapped buffer" error
+unsigned run3(Context cl, cl_kernel kernel,
+              cl_mem d_a, cl_mem d_b, cl_mem d_c, size_t N)
+{
+  cl_int err;
+  float *h_a, *h_b, *h_c;
+  size_t dataSize = N*sizeof(cl_float);
+
+  // Initialise data
+  srand(0);
+  h_a = clEnqueueMapBuffer(cl.queue, d_a, CL_TRUE,
+                           CL_MAP_WRITE_INVALIDATE_REGION,
+                           0, dataSize, 0, NULL, NULL, &err);
+  checkError(err, "mapping d_a buffer");
+  h_b = clEnqueueMapBuffer(cl.queue, d_b, CL_TRUE,
+                           CL_MAP_WRITE_INVALIDATE_REGION,
+                           0, dataSize, 0, NULL, NULL, &err);
+  checkError(err, "mapping d_b buffer");
+  h_c = clEnqueueMapBuffer(cl.queue, d_c, CL_FALSE, CL_MAP_READ, 0, dataSize,
+                           0, NULL, NULL, &err);
+  checkError(err, "mapping d_c buffer");
   for (unsigned i = 0; i < N; i++)
   {
     h_a[i] = rand()/(float)RAND_MAX;
@@ -77,6 +182,71 @@ int main(int argc, char *argv[])
                                1, NULL, &N, NULL, 0, NULL, NULL);
   checkError(err, "enqueuing kernel");
 
+  err = clFinish(cl.queue);
+  checkError(err, "running kernel");
+
+  unsigned errors = checkResults(N, h_a, h_b, h_c);
+
+  err = clEnqueueUnmapMemObject(cl.queue, d_c, h_c, 0, NULL, NULL);
+  checkError(err, "unmapping d_c");
+
+  return errors;
+}
+
+// Re-map input buffers for reading
+// Should not result in any error
+unsigned run4(Context cl, cl_kernel kernel,
+              cl_mem d_a, cl_mem d_b, cl_mem d_c, size_t N)
+{
+  cl_int err;
+  float *h_a, *h_b, *h_c;
+  size_t dataSize = N*sizeof(cl_float);
+
+  // Initialise data
+  srand(0);
+  h_a = clEnqueueMapBuffer(cl.queue, d_a, CL_TRUE,
+                           CL_MAP_WRITE_INVALIDATE_REGION,
+                           0, dataSize, 0, NULL, NULL, &err);
+  checkError(err, "mapping d_a buffer");
+  h_b = clEnqueueMapBuffer(cl.queue, d_b, CL_TRUE,
+                           CL_MAP_WRITE_INVALIDATE_REGION,
+                           0, dataSize, 0, NULL, NULL, &err);
+  checkError(err, "mapping d_b buffer");
+  h_c = clEnqueueMapBuffer(cl.queue, d_c, CL_FALSE, CL_MAP_READ, 0, dataSize,
+                           0, NULL, NULL, &err);
+  checkError(err, "mapping d_c buffer");
+  for (unsigned i = 0; i < N; i++)
+  {
+    h_a[i] = rand()/(float)RAND_MAX;
+    h_b[i] = rand()/(float)RAND_MAX;
+    h_c[i] = 0;
+  }
+
+  h_a = clEnqueueMapBuffer(cl.queue, d_a, CL_TRUE,
+                           CL_MAP_READ,
+                           0, dataSize, 0, NULL, NULL, &err);
+  checkError(err, "mapping d_a buffer");
+  h_b = clEnqueueMapBuffer(cl.queue, d_b, CL_TRUE,
+                           CL_MAP_READ,
+                           0, dataSize, 0, NULL, NULL, &err);
+  checkError(err, "mapping d_b buffer");
+
+  err = clEnqueueUnmapMemObject(cl.queue, d_a, h_a, 0, NULL, NULL);
+  checkError(err, "unmapping d_a");
+  err = clEnqueueUnmapMemObject(cl.queue, d_b, h_b, 0, NULL, NULL);
+  checkError(err, "unmapping d_b");
+  err = clEnqueueUnmapMemObject(cl.queue, d_c, h_c, 0, NULL, NULL);
+  checkError(err, "unmapping d_c");
+
+  err  = clSetKernelArg(kernel, 0, sizeof(cl_mem), &d_a);
+  err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &d_b);
+  err |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &d_c);
+  checkError(err, "setting kernel args");
+
+  err = clEnqueueNDRangeKernel(cl.queue, kernel,
+                               1, NULL, &N, NULL, 0, NULL, NULL);
+  checkError(err, "enqueuing kernel");
+
   h_c = clEnqueueMapBuffer(cl.queue, d_c, CL_FALSE, CL_MAP_READ, 0, dataSize,
                            0, NULL, NULL, &err);
   checkError(err, "mapping d_c buffer");
@@ -84,25 +254,46 @@ int main(int argc, char *argv[])
   err = clFinish(cl.queue);
   checkError(err, "running kernel");
 
-  // Check results
-  unsigned errors = 0;
-  for (unsigned i = 0; i < N; i++)
-  {
-    float ref = h_a[i] + h_b[i];
-    if (fabs(ref - h_c[i]) > TOL)
-    {
-      if (errors < MAX_ERRORS)
-      {
-        fprintf(stderr, "%4d: %.4f != %.4f\n", i, h_c[i], ref);
-      }
-      errors++;
-    }
-  }
-  if (errors)
-    printf("%d errors detected\n", errors);
+  unsigned errors = checkResults(N, h_a, h_b, h_c);
 
-  clEnqueueUnmapMemObject(cl.queue, d_c, h_c, 0, NULL, NULL);
+  err = clEnqueueUnmapMemObject(cl.queue, d_c, h_c, 0, NULL, NULL);
   checkError(err, "unmapping d_c");
+
+  return errors;
+}
+
+int main(int argc, char *argv[])
+{
+  cl_int err;
+  cl_kernel kernel;
+  cl_mem d_a, d_b, d_c;
+
+  size_t N = 1;
+  if (argc > 1)
+  {
+    N = atoi(argv[1]);
+  }
+
+  Context cl = createContext(KERNEL_SOURCE);
+
+  kernel = clCreateKernel(cl.program, "vecadd", &err);
+  checkError(err, "creating kernel");
+
+  size_t dataSize = N*sizeof(cl_float);
+
+  d_a = clCreateBuffer(cl.context, CL_MEM_READ_ONLY, dataSize, NULL, &err);
+  checkError(err, "creating d_a buffer");
+  d_b = clCreateBuffer(cl.context, CL_MEM_READ_ONLY, dataSize, NULL, &err);
+  checkError(err, "creating d_b buffer");
+  d_c = clCreateBuffer(cl.context, CL_MEM_WRITE_ONLY, dataSize, NULL, &err);
+  checkError(err, "creating d_c buffer");
+
+  unsigned errors = 0;
+
+  errors += run1(cl, kernel, d_a, d_b, d_c, N);
+  errors += run2(cl, kernel, d_a, d_b, d_c, N);
+  errors += run3(cl, kernel, d_a, d_b, d_c, N);
+  errors += run4(cl, kernel, d_a, d_b, d_c, N);
 
   clReleaseMemObject(d_a);
   clReleaseMemObject(d_b);
@@ -111,4 +302,26 @@ int main(int argc, char *argv[])
   releaseContext(cl);
 
   return (errors != 0);
+}
+
+unsigned checkResults(size_t N, float *a, float *b, float *results)
+{
+  // Check results
+  unsigned errors = 0;
+  for (unsigned i = 0; i < N; i++)
+  {
+    float ref = a[i] + b[i];
+    if (fabs(ref - results[i]) > TOL)
+    {
+      if (errors < MAX_ERRORS)
+      {
+        fprintf(stderr, "%4d: %.4f != %.4f\n", i, results[i], ref);
+      }
+      errors++;
+    }
+  }
+  if (errors)
+    printf("%d errors detected\n", errors);
+
+  return errors;
 }
