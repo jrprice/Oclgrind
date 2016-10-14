a5df5e29c102de241d966ff5ce09e9961a25e79f
James Price <j.price@bristol.ac.uk>
2016-10-14 20:55:06 +0100

    Add check for MSVCP140.dll in install.bat

--- src/install/install.bat
+++ src/install/install.bat
@@ -13,6 +13,19 @@ xcopy uninstall.bat "%ROOT%\"           /Y    || goto :error
 
 regedit /S oclgrind-icd.reg                   || goto :error
 
+echo.
+echo Installation completed.
+echo.
+
+if not exist C:\Windows\system32\msvcp140.dll (
+  echo WARNING: MSVCP140.dll not found - Oclgrind may fail to work correctly
+  echo Download the Microsoft Visual C++ Redistributable from here:
+  echo.
+  echo   https://www.microsoft.com/en-us/download/details.aspx?id=48145
+  echo.
+  pause
+)
+
 goto :EOF
 
 
