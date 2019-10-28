9a71682ca86a9a3438a982d39563f42fb9fa1724
James Price <jamesprice.dev@gmail.com>
2019-10-28 09:40:53 -0400

    Update copyright year

--- src/kernel/oclgrind-kernel.cpp
+++ src/kernel/oclgrind-kernel.cpp
@@ -1,5 +1,5 @@
 // main.cpp (Oclgrind)
-// Copyright (c) 2013-2016, James Price and Simon McIntosh-Smith,
+// Copyright (c) 2013-2019, James Price and Simon McIntosh-Smith,
 // University of Bristol. All rights reserved.
 //
 // This program is provided under a three-clause BSD license. For full
@@ -193,7 +193,7 @@ static bool parseArguments(int argc, char *argv[])
       cout << endl;
       cout << "Oclgrind " PACKAGE_VERSION << endl;
       cout << endl;
-      cout << "Copyright (c) 2013-2018" << endl;
+      cout << "Copyright (c) 2013-2019" << endl;
       cout << "James Price and Simon McIntosh-Smith, University of Bristol"
            << endl;
       cout << "https://github.com/jrprice/Oclgrind" << endl;
